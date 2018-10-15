1de0db0a9f45924a008c7fb418b63bd9fa83af89
王东鸿 <1366678737@qq.com>
2018-10-15 08:17:31 +0800

    no message

---
+++ record-all/java8-test/src/test/java/com/krista/java8/test/AppTest.java
@@ -0,0 +1,17 @@
+package com.krista.java8.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
