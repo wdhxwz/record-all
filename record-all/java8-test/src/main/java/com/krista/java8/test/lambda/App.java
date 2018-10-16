04d26b9e81802816b2b30d9044cb8880b6c498b4
王东鸿 <1366678737@qq.com>
2018-10-16 21:19:25 +0800

    lambda

--- record-all/java8-test/src/main/java/com/krista/java8/test/lambda/App.java
+++ record-all/java8-test/src/main/java/com/krista/java8/test/lambda/App.java
@@ -1,7 +1,9 @@
 package com.krista.java8.test.lambda;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * @Auther: dw_wanghonghong
@@ -10,7 +12,8 @@ import java.util.function.Consumer;
  */
 public class App {
     public static void main(String[] args) {
-        Arrays.asList("sd","fdsf","3e2","ew3").forEach(
+        List<String> list = Arrays.asList("sd", "fdsf", "3e2", "ew3");
+        list.forEach(
                 new Consumer<String>() {
                     @Override
                     public void accept(String element) {
@@ -20,13 +23,26 @@ public class App {
         );
 
         // lambda
-        Arrays.asList("sd","fdsf","3e2","ew3").forEach(
+        list.forEach(
                 element -> System.out.println(element)
         );
 
         // 方法引用
-        Arrays.asList("sd","fdsf","3e2","ew3").forEach(
+        list.forEach(
                 System.out::println
         );
+
+        String message = "Hello lambda";
+        // message = "";
+        Runnable runnable = () -> {
+            System.out.println(message);
+            // message = "";
+        };
+        runnable.run();
+
+        final String[] array = {"Hello", "world"};
+
+        Predicate<Integer> atLeast5 = x -> x > 5;
+        System.out.println(atLeast5.test(5));
     }
 }
