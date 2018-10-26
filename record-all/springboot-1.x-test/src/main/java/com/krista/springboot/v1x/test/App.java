93f39e094524cf7b0be3e5fee33b3322de3621e3
wangdh <1366678737@qq.com>
2018-10-26 18:34:10 +0800

    监听事件

--- record-all/springboot-1.x-test/src/main/java/com/krista/springboot/v1x/test/App.java
+++ record-all/springboot-1.x-test/src/main/java/com/krista/springboot/v1x/test/App.java
@@ -1,5 +1,9 @@
 package com.krista.springboot.v1x.test;
 
+import com.netflix.discovery.DiscoveryManager;
+import com.netflix.discovery.guice.EurekaModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -14,7 +18,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            DiscoveryManager.getInstance().shutdownComponent();
+            logger.info("jvm is stopping...");
+        }));
         SpringApplication.run(App.class, args);
     }
 }
