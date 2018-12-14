8df2463c5cdebb1467a3c0f6acffcac6e230dde8
wangdh <1366678737@qq.com>
2018-12-14 17:47:35 +0800

    2.x

---
+++ record-all/SpringBoot2.x/springboot-with-view/src/main/java/com/krista/eagle/admin/AdminApplication.java
@@ -0,0 +1,18 @@
+package com.krista.eagle.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AdminApplication
+ *
+ * @author dw_wangdonghong
+ * @version V1.0
+ * @since 2018/12/14 17:38
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
