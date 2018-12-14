ddd59fc5d36dfccbf4d65328c6fc0f4acda37c39
wangdh <1366678737@qq.com>
2018-12-14 18:09:43 +0800

    创建目录

---
+++ record-all/SpringBoot2.x/springboot-with-view/src/main/java/com/krista/eagle/admin/configuration/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @Auther: dw_wanghonghong
+ * @Date: 2018/12/14 18:07
+ * @Description:
+ */
+package com.krista.eagle.admin.configuration;
---
+++ record-all/SpringBoot2.x/springboot-with-view/src/main/java/com/krista/eagle/admin/controller/rpc/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @Auther: dw_wanghonghong
+ * @Date: 2018/12/14 18:08
+ * @Description:
+ */
+package com.krista.eagle.admin.controller.rpc;
