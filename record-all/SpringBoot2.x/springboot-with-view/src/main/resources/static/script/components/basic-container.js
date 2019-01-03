4539a83718f0e0f0bb6ffb584b9f5a7f22ef4073
wangdh <1366678737@qq.com>
2019-01-03 16:48:25 +0800

    添加基础错误页面

---
+++ record-all/SpringBoot2.x/springboot-with-view/src/main/resources/static/script/components/basic-container.js
@@ -0,0 +1,21 @@
+(function ($, Vue, axios) {
+    'use strict';
+    // 注册Vue组件
+    Vue.component("basic-container", function (resolve) {
+        axios.get("tpls/basic-container.html", {
+            accept: 'text/html, text/plain'
+        }).then(function (data) {
+            resolve(vueOpts(data.data));
+        }).catch(function (error) {
+            console.log(error);
+        });
+
+        function vueOpts(htmlTpl) {
+            var options = {
+                template: htmlTpl
+            };
+
+            return options;
+        }
+    });
+}(jQuery, Vue, axios));
