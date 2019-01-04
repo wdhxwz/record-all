73e1c9a78f4cccd0340cae100172f2373f3a50ad
wangdh <1366678737@qq.com>
2019-01-04 15:27:55 +0800

    233

---
+++ record-all/SpringBoot2.x/springboot-with-view/src/main/resources/static/components/basic-container.vue
@@ -0,0 +1,18 @@
+<div class="basic-container">
+    <slot></slot>
+</div>
+
+<script>
+    module.exports = {
+        name: "basic-container"
+    }
+</script>
+
+<style>
+    .basic-container {
+        padding: 20px;
+        margin-bottom: 20px;
+        background-color: #fff;
+        border-radius: 6px;
+    }
+</style>
--- record-all/SpringBoot2.x/springboot-with-view/src/main/resources/static/script/global.js
+++ record-all/SpringBoot2.x/springboot-with-view/src/main/resources/static/script/global.js
@@ -3,4 +3,5 @@
     // 注册Vue组件
     httpVueLoader.register(Vue, "../../components/menu-icon.vue");
     httpVueLoader.register(Vue, "../../components/sidebar-menu.vue");
+    httpVueLoader.register(Vue, "../../components/basic-container.vue");
 }(jQuery, Vue));
